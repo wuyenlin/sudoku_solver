cf3012911abd37277a1695509d7eead18b09a383
Yen-Lin Wu <finne0831@gmail.com>
2021-10-01 14:59:59 +0200

    overhauled func arg

--- solver.py
+++ solver.py
@@ -2,9 +2,14 @@
 
 import numpy as np
 
+def blk_index(i, j):
+    return (i//3) * 3 + j//3
+
+
 class Solver:
     def __init__(self, all: list) -> np.array:
-        self.all = all
+        self.original = all
+
 
     def get_blks(self, all):
         blks = []
@@ -17,19 +22,16 @@ class Solver:
                 blks.append(blk)
         return np.array(blks)
 
-    def initialize(self):
+
+    def initialize(self, all):
         """get rows, columns and blocks"""
-        self.rows = np.array(self.all)
-        self.cols = np.array(self.all).T
-        self.blks = self.get_blks(self.all)
+        self.rows = np.array(all)
+        self.cols = np.array(all).T
+        self.blks = self.get_blks(all)
 
 
-
-    def get_candidates(self):
-        def blk_index(i, j):
-            return (i//3) * 3 + j//3
-        
-        self.initialize()
+    def get_candidates(self, all):
+        self.initialize(all)
         candidates = []
         for i in range(9):
             row_candidates = []
@@ -50,12 +52,63 @@ class Solver:
 
 
     def solve(self):
-        #TODO complete solver ###
-        pass
+        puzzle = self.original.copy()
+        self.get_candidates(puzzle)
+        print(self.candidates)
+        to_fill = True
+        while to_fill:
+            to_fill = False
+            for i in range(9):
+                for j in range(9):
+                    if len(self.candidates[i][j]) == 1 and puzzle[i][j] == 0:
+                        puzzle[i][j] = self.candidates[i][j][0]
+                        self.get_candidates(puzzle)
+                        to_fill = True
+        self.puzzle = puzzle
+    
+
+    def correct(self, puzzle):
+        """ a boolean function that determines if the puzzle is solved"""
+        if np.all(np.sum(puzzle, axis=1) == 45) and \
+            np.all(np.sum(puzzle, axis=0) == 45) and \
+            np.all(np.sum(self.get_blks(puzzle), axis=1) == 45):
+            return True
+        return False
+
+
+    def valid(self, puzzle):
+        self.get_candidates(puzzle)
+        for i in range(9):
+            for j in range(9):
+                if len(self.candidates[i][j]) == 0:
+                    return False
+        return True
+
+
+    def filter(self):
+        from copy import deepcopy
+        # Check for empty cells
+        test = self.original.copy()
+        self.get_candidates(test)
+        filtered_candidates = deepcopy(self.candidates)
+        for i in range(9):
+            for j in range(9):
+                if test[i][j] == 0:
+                    for candidate in self.candidates[i][j]:
+                        # Use test candidate
+                        test[i][j] = candidate
+                        # Remove candidate if it produces an invalid grid
+                        if not is_valid_grid(self.solve(test)):
+                            filtered_candidates[i][j].remove(candidate)
+                        # Revert changes
+                        test[i][j] = 0
+
+
+    def main(self):
+        self.solve()
 
 
 if __name__ == "__main__":
-
     r1 = [0,0,0,5,0,4,0,2,0]
     r2 = [0,3,0,2,0,6,8,0,0]
     r3 = [0,1,0,0,9,0,0,4,0]
@@ -65,8 +118,6 @@ if __name__ == "__main__":
     r7 = [0,0,3,4,0,0,0,0,0]
     r8 = [0,0,0,0,5,7,0,0,0]
     r9 = [0,4,8,1,6,3,0,0,0]
-    all = [r1, r2, r3, r4, r5, r6, r7, r8, r9]
-    s = Solver(all)
-    s.initialize()
-    s.get_candidates()
-    print(s.candidates)
+    puzzle = [r1, r2, r3, r4, r5, r6, r7, r8, r9]
+    s = Solver(puzzle)
+    s.main()
