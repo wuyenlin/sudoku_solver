e9f77d34627e641ca8065780b2a6cf0b2cb37f6f
Yen-Lin Wu <finne0831@gmail.com>
2021-10-06 11:46:16 +0200

    cleaned up

--- train.py
+++ train.py
@@ -13,8 +13,8 @@ from utils.model import Net
 """
 This script is borrowed and modified from
 https://github.com/pytorch/examples/blob/master/mnist/main.py
-Since the original neural network was giving an accuracy of 10%, I have
-built my own model in `utils.model.py`.
+Since the original neural network was giving low accuracy, 
+I have built my own model in `utils.model.py`.
 """
 
 
@@ -28,10 +28,10 @@ def train(args, model, device, train_loader, optimizer, epoch):
         loss.backward()
         optimizer.step()
         if batch_idx % args.log_interval == 0:
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+            print("Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}".format(
                 epoch, batch_idx * len(data), len(train_loader.dataset),
                 100. * batch_idx / len(train_loader), loss.item()))
-            if args.dry_run:
+            if args.single:
                 break
 
 
@@ -43,65 +43,67 @@ def test(model, device, test_loader):
         for data, target in test_loader:
             data, target = data.to(device), target.to(device)
             output = model(data)
-            test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
+            test_loss += F.nll_loss(output, target, reduction="sum").item()  # sum up batch loss
             pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
             correct += pred.eq(target.view_as(pred)).sum().item()
 
     test_loss /= len(test_loader.dataset)
 
-    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+    print("\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n".format(
         test_loss, correct, len(test_loader.dataset),
         100. * correct / len(test_loader.dataset)))
 
 
 def main():
     # Training settings
-    parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
-    parser.add_argument('--batch-size', type=int, default=64, metavar='N',
-                        help='input batch size for training (default: 64)')
-    parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
-                        help='input batch size for testing (default: 1000)')
-    parser.add_argument('--epochs', type=int, default=14, metavar='N',
-                        help='number of epochs to train (default: 14)')
-    parser.add_argument('--lr', type=float, default=1.0, metavar='LR',
-                        help='learning rate (default: 1.0)')
-    parser.add_argument('--gamma', type=float, default=0.7, metavar='M',
-                        help='Learning rate step gamma (default: 0.7)')
-    parser.add_argument('--no-cuda', action='store_true', default=False,
-                        help='disables CUDA training')
-    parser.add_argument('--dry-run', action='store_true', default=False,
-                        help='quickly check a single pass')
-    parser.add_argument('--seed', type=int, default=1, metavar='S',
-                        help='random seed (default: 1)')
-    parser.add_argument('--log-interval', type=int, default=10, metavar='N',
-                        help='how many batches to wait before logging training status')
-    parser.add_argument('--save-model', action='store_true', default=False,
-                        help='For Saving the current Model')
+    parser = argparse.ArgumentParser(description="PyTorch MNIST Example")
+    parser.add_argument("--bs", type=int, default=64, metavar="N",
+                        help="input batch size for training (default: 64)")
+    parser.add_argument("--test_bs", type=int, default=1000, metavar="N",
+                        help="input batch size for testing (default: 1000)")
+    parser.add_argument("--epochs", type=int, default=14, metavar="N",
+                        help="number of epochs to train (default: 14)")
+    parser.add_argument("--lr", type=float, default=1.0, metavar="LR",
+                        help="learning rate (default: 1.0)")
+    parser.add_argument("--gamma", type=float, default=0.7, metavar="M",
+                        help="Learning rate step gamma (default: 0.7)")
+    parser.add_argument("--no_cuda", action="store_true", default=False,
+                        help="disables CUDA training")
+    parser.add_argument("--single", action="store_true", default=False,
+                        help="quickly check a single pass")
+    parser.add_argument("--seed", type=int, default=1, metavar="S",
+                        help="random seed (default: 1)")
+    parser.add_argument("--log_interval", type=int, default=10, metavar="N",
+                        help="how many batches to wait before logging training status")
+    parser.add_argument("--save_model", action="store_true", default=False,
+                        help="For Saving the current Model")
+    parser.add_argument("--export_training_curves", action="store_true", 
+                        help="Save train/val curves in .png file")
     args = parser.parse_args()
     use_cuda = not args.no_cuda and torch.cuda.is_available()
-
     torch.manual_seed(args.seed)
 
     device = torch.device("cuda" if use_cuda else "cpu")
 
-    train_kwargs = {'batch_size': args.batch_size}
-    test_kwargs = {'batch_size': args.test_batch_size}
+    train_kwargs = {"batch_size": args.bs}
+    test_kwargs = {"batch_size": args.test_bs}
     if use_cuda:
-        cuda_kwargs = {'num_workers': 1,
-                       'pin_memory': True,
-                       'shuffle': True}
+        cuda_kwargs = {"num_workers": 1,
+                       "pin_memory": True,
+                       "shuffle": True}
         train_kwargs.update(cuda_kwargs)
         test_kwargs.update(cuda_kwargs)
 
-    transform=transforms.Compose([
+    transform = transforms.Compose([
+        transforms.Resize([28,28]),
         transforms.ToTensor(),
         transforms.Normalize((0.1307,), (0.3081,))
         ])
-    dataset1 = datasets.MNIST('../data', train=True, download=True,
+    dataset1 = datasets.MNIST("../data", train=True, download=True,
                        transform=transform)
-    dataset2 = datasets.MNIST('../data', train=False,
+    dataset2 = datasets.MNIST("../data", train=False,
                        transform=transform)
-    train_loader = torch.utils.data.DataLoader(dataset1,**train_kwargs)
+    train_loader = torch.utils.data.DataLoader(dataset1, **train_kwargs)
     test_loader = torch.utils.data.DataLoader(dataset2, **test_kwargs)
 
     model = Net().to(device)
@@ -113,9 +115,28 @@ def main():
         test(model, device, test_loader)
         scheduler.step()
 
+        # TODO: fix function
+        if args.export_training_curves and epochs > 3:
+            import matplotlib.pyplot as plt
+            import matplotlib
+            matplotlib.use("Agg")
+            plt.figure()
+            epoch_x = np.arange(3, len(losses_3d_train)) + 1
+            plt.plot(epoch_x, losses_3d_train[3:], "--", color="C0")
+            plt.plot(epoch_x, losses_3d_valid[3:], color="C1")
+            plt.legend(["", ""])
+            plt.ylabel("Accuracy (%)")
+            plt.xlabel("Epoch")
+            plt.xlim((3, epoch))
+            plt.savefig("./accuracy.png")
+            plt.close("all")
+
+
     if args.save_model:
-        torch.save(model.state_dict(), "mnist_cnn.pth")
+        save_path = "./mnist_cnn.pth"
+        torch.save(model.state_dict(), save_path)
+        print("Parameters saved to ", save_path)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
