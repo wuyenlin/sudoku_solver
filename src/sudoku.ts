ef7c140561de1ed6134e28cdabbdb4e5b222b238
Yen-Lin Wu <finne0831@gmail.com>
2021-11-09 11:43:34 +0100

    overhauled the code

--- src/sudoku.ts
+++ src/sudoku.ts
@@ -1,49 +1,20 @@
-function is_valid(puzzle: number[][], row: number, col: number, cand: number): boolean{
-    // check for repetition in row and column
-    for (var x=0; x<9; x++){
-        if (puzzle[row][x]==cand || puzzle[x][col] == cand) return false;
-    }
-
-    // check in subgrid
-    var starting_row: number = row - row%3;
-    var starting_col: number = col - col%3;
-
-    for (var i=0; i<3; i++){
-        for (var j=0; j<3; j++){
-            if (puzzle[i+starting_row][j+starting_col] == cand) return false;
-        }
-    }
-    return true;
-}
-
-function find_solution(puzzle: number[][], row: number, col: number): boolean{
-    if (row==8 && col==9){
-        console.log("Solved!");
-        return true;
-    }
-
-    // proceed to next row
-    if (col==9){
-        row += 1;
-        col = 0;
-    }
-    if (puzzle[row][col] > 0) return find_solution(puzzle, row, col+1);
-
-    for (var cand=1; cand<10; cand++){
-        if (is_valid(puzzle, row, col, cand)){
-            puzzle[row][col] = cand;
-            if (find_solution(puzzle, row, col+1)) return true;
-        }
-        // revert to 0 if the above assumption is wrong
-        puzzle[row][col] = 0;
-    }
-    return false;
-}
-
-
 type Puzzle = {r1: string, r2: string, r3: string, r4: string, r5: string,
             r6: string, r7: string, r8: string, r9: string};
 
+function repeat_string(string: string, times: number): string{
+    var repeated: string = "";
+    while (times > 0){
+        repeated += string;
+        times--;
+    }
+    return repeated
+}
+
+function visualize(puzzle: number[][]): string{
+    var formatted: string = puzzle.toString();
+    /** convert to string array and add | and \n to visualize the grid properly */
+    return formatted;
+}
 
 class Sudoku {
     private puzzle: HTMLInputElement[];
@@ -63,26 +34,69 @@ class Sudoku {
     
     processId(elements: HTMLInputElement[]): number[][]{
         var arrays: number[][] = [];
-        for (var i=0; i<10; i++) arrays.push((elements[i].value).split("").map(Number));
+        for (var i=0; i<9; i++) arrays.push((elements[i].value).split("").map(Number));
+        console.log(arrays);
         return arrays
+    }
+
+    is_valid(puzzle: number[][], row: number, col: number, cand: number): boolean{
+        // check for repetition in row and column
+        for (var x=0; x<9; x++){
+            if (puzzle[row][x]==cand || puzzle[x][col] == cand) return false;
+        }
+
+        // check in subgrid
+        var starting_row: number = row - row%3;
+        var starting_col: number = col - col%3;
+
+        for (var i=0; i<3; i++){
+            for (var j=0; j<3; j++){
+                if (puzzle[i+starting_row][j+starting_col] == cand) return false;
+            }
+        }
+        return true;
+    }
+
+    find_solution(puzzle: number[][], row: number, col: number): boolean{
+        if (row==8 && col==9){
+            console.log("Solved!");
+            return true;
+        }
+
+        // proceed to next row
+        if (col==9){
+            row += 1;
+            col = 0;
+        }
+        if (puzzle[row][col] > 0) return this.find_solution(puzzle, row, col+1);
+
+        for (var cand=1; cand<10; cand++){
+            if (this.is_valid(puzzle, row, col, cand)){
+                puzzle[row][col] = cand;
+                if (this.find_solution(puzzle, row, col+1)) return true;
+            }
+            // revert to 0 if the above assumption is wrong
+            puzzle[row][col] = 0;
+        }
+        return false;
+    }
+
+    solve(puzzle: number[][]): string{
+        if (this.find_solution(puzzle, 0, 0)) {
+            console.log(puzzle);
+            // return "Solved";
+            return puzzle.toString();
+        } else{
+            return "Unsolved!";
+        }
     }
 
     wireEvents(){
         document.getElementById("Solve")!.addEventListener("click",
             event => {
-                // this.output.innerHTML = this.Solve(this.processId(this.puzzle)).toString();
-                this.output.innerHTML = this.solve(this.processId(this.puzzle));
+                var parsedPuzzle: number[][] = this.processId(this.puzzle);
+                this.output.innerHTML = this.solve(parsedPuzzle);
             });
-    }
-
-
-    solve(puzzle: number[][]): string{
-        if (find_solution(puzzle, 0, 0)) {
-            console.log(puzzle);
-            return "Solved";
-        } else{
-            return "Unsolved!";
-        }
     }
 }
 
